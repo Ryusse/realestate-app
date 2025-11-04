2132de1e3a421516c50a4e4e88d4165031b7ddf2
Isaac Alvarez <157077804+isAlvarezDev@users.noreply.github.com>
2025-11-04 15:50:06 -0400

    fix(frontend): formatted cardDomicileProp and cardDomicileData

--- frontend/src/modules/marketing/components/CardDomicileProp.tsx
+++ frontend/src/modules/marketing/components/CardDomicileProp.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
-import { domicileProp } from "./cardDomicileData";
+
 import { MapPin } from "lucide-react";
-export const CardDomicileProps = () => {
+
+import { domicileProp } from "./cardDomicileData";
+export const CardDomicileProp = () => {
 	return (
 		<article className="bg-white rounded-sm p-3 space-y-2">
 			<Image
@@ -11,12 +13,10 @@ export const CardDomicileProps = () => {
 				width={300}
 				height={200}
 			/>
-				<h1 className="font-bold text-lg">
-					{domicileProp.titleProperty}
-				</h1>
-				<p className="text-lg text-green-700 font-bold rounded-full">
-					{domicileProp.price}
-				</p>
+			<h1 className="font-bold text-lg">{domicileProp.titleProperty}</h1>
+			<p className="text-lg text-green-700 font-bold rounded-full">
+				{domicileProp.price}
+			</p>
 			<div className="flex items-center">
 				<MapPin className="text-gray-600 mr-1" size={15} />
 				<p className="text-gray-600 text-sm">{domicileProp.address}</p>
--- frontend/src/modules/marketing/components/cardDomicileData.ts
+++ frontend/src/modules/marketing/components/cardDomicileData.ts
@@ -1,13 +1,16 @@
 interface DomicileProp {
-    titleProperty: string;
-    price: string;
-    address: string;
-    thumbnail: string
+	titleProperty: string;
+	price: string;
+	address: string;
+	thumbnail: string;
 }
 
 export const domicileProp: DomicileProp = {
-    titleProperty: "Serenity Cottage Premium Caves",
-    price: (440000).toLocaleString('en-US', {style: 'currency', currency: 'USD'}),
-    address: '123 Main St, Tulsa, OK 74136',
-    thumbnail: '/images/american-house.webp'
-}
+	titleProperty: "Serenity Cottage Premium Caves",
+	price: (440000).toLocaleString("en-US", {
+		style: "currency",
+		currency: "USD",
+	}),
+	address: "123 Main St, Tulsa, OK 74136",
+	thumbnail: "/images/american-house.webp",
+};
