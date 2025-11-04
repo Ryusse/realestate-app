16448fde47b352aea79a0a03cd73bf58f36dc1ef
jocano <joelangelocano@gmail.com>
2025-11-03 22:27:49 -0500

    style(frontend): improve imports formatting

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
 import { Toaster } from "@src/components/ui/sonner";
 
 const geistSans = Geist({
